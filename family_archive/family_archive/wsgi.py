14f99693b9d47777e12e96acc06cb04de29ed961
vlgreb <vlgrebennikov@mail.ru>
2022-05-10 16:09:48 +0300

    start_project

---
+++ family_archive/family_archive/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for family_archive project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'family_archive.settings')
+
+application = get_wsgi_application()
